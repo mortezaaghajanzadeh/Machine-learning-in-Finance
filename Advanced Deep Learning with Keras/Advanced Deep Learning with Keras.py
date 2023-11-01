5dbb74f4041683d9f71d0eaed8e07194d119048e
Seyyed Morteza Aghajanzadeh <mortezaaghajanzadeh@gmail.com>
2023-11-01 09:08:55 +0100

    course progress

--- Advanced Deep Learning with Keras/Advanced Deep Learning with Keras.py
+++ Advanced Deep Learning with Keras/Advanced Deep Learning with Keras.py
@@ -1,4 +1,21 @@
 #%%
 import pandas as pd
-from tensorflow.keras.layers import Dense, Dropout, Input
-#%%
+from tensorflow.keras.layers import Dense, Input, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import plot_model
+#%% Input
+input_tensor = Input(shape=(1,))
+print(input_tensor)
+#%% Ouput
+output_layer = Dense(1,name = 'Predicted-Score-Diff')
+output_tensor = output_layer(input_tensor)
+#%% Model
+model = Model(input_tensor, output_tensor)
+model.compile(optimizer='adam', loss='mae')
+print(model.summary())
+plot_model(model, to_file='model.png')
+# %%
+import matplotlib.pyplot as plt
+img = plt.imread('model.png')
+plt.imshow(img)
+plt.show()
